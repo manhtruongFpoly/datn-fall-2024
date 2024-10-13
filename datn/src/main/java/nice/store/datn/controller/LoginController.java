fad1caa90581b4f351424c8bc8feeb3b76055381
xtruongx <xtruongx932@gmail.com>
2024-10-13 22:54:36 +0700

    push

---
+++ datn/src/main/java/nice/store/datn/controller/LoginController.java
@@ -0,0 +1,18 @@
+package nice.store.datn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/admin")
+    public String adminSubmit(){
+        return "pages/landing_page";
+    }
+
+
+    @RequestMapping("/home")
+    public String loginSubmit(){
+        return "index";
+    }
+}
