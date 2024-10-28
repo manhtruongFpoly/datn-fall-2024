cd6283a0494d9ea16bf9bdbbc63a65e8a8d98cae
DESKTOP-3T06CH1\LAPTOP24H <hainvph22238@fpt.edu.vn>
2024-10-28 22:16:13 +0700

    update

---
+++ datn/src/main/java/nice/store/datn/repository/PhieuGiamGiaRepository.java
@@ -0,0 +1,10 @@
+package nice.store.datn.repository;
+
+import nice.store.datn.entity.PhieuGiamGia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhieuGiamGiaRepository extends JpaRepository<PhieuGiamGia , Integer> {
+
+
+
+}
